a37adabb0e6ade6515adbd908617349cae31266b
ghl <18296743174@163.com>
2021-09-18 14:16:56 +0800

    StreamTest

--- src/test/java/cn/ghl/test/StreamTest.java
+++ src/test/java/cn/ghl/test/StreamTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest(classes = TestApp.class)
 public class StreamTest {
@@ -14,8 +15,13 @@ public class StreamTest {
     @Test
     public void streamTest(){
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        numbers.parallelStream()
-                .forEach(System.out::println);
+//        numbers.parallelStream()
+//                .forEach(System.out::println);
+
+        List<Integer> list = numbers.stream().filter(e -> numbers.indexOf(e) > 3)
+                .collect(Collectors.toList());
+        list.forEach(System.out::print);
 //        ForkJoinTask
+//        int i = Integer.parseInt("11");
     }
 }
